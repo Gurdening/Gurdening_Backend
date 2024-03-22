f691c10d20d7c6de4ba34523e28db897d1ab36c1
Junyeong-An <2117ab@gmail.com>
2024-03-23 00:04:55 +0900

    Initial setting

---
+++ src/main/java/com/gurdening_backend/GurdeningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gurdening_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurdeningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurdeningBackendApplication.class, args);
+	}
+
+}
